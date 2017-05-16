3cd60d8558ada8181fbda1ef9278a676b82b17f3
Vadim Chernyshov <senteris@yandex.ru>
2017-05-16 23:41:10 +0300

    Added DashboardUrl entity(getters and setters)

--- src/main/java/ru/ncteam/levelchat/entity/DashboardUrl.java
+++ src/main/java/ru/ncteam/levelchat/entity/DashboardUrl.java
@@ -20,4 +20,35 @@ class DashboardUrl {
     @JoinColumn(name = "INTEREST_ID", nullable = false)
     private Interests interests;
 
+    public long getBoardId() {
+        return boardId;
+    }
+
+    public void setBoardId(long boardId) {
+        this.boardId = boardId;
+    }
+
+    public String getUrlInterest() {
+        return urlInterest;
+    }
+
+    public void setUrlInterest(String urlInterest) {
+        this.urlInterest = urlInterest;
+    }
+
+    public String getStatusChat() {
+        return statusChat;
+    }
+
+    public void setStatusChat(String statusChat) {
+        this.statusChat = statusChat;
+    }
+
+    public Interests getInterests() {
+        return interests;
+    }
+
+    public void setInterests(Interests interests) {
+        this.interests = interests;
+    }
 }
